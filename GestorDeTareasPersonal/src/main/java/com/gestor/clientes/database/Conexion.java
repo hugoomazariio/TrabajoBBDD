4ea34ff16d1d365acf879506917ef242a278787f
Hugo <160237140+hugoomazariio@users.noreply.github.com>
2024-04-25 08:56:15 +0200

    .

--- GestorDeTareasPersonal/src/main/java/com/gestor/clientes/database/Conexion.java
+++ GestorDeTareasPersonal/src/main/java/com/gestor/clientes/database/Conexion.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    String url = "jdbc:mysql://localhost:3306/dbgestor";
+    String url = "jdbc:mysql://localhost:3306/dbclientes";
     String user = "root";
     String pass = "";
     Connection c;
